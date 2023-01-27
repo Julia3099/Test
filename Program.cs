2c2511f1b656254ab5dd222c9ff021d2ba771f5a
Julia3099 <121186506+Julia3099@users.noreply.github.com>
2023-01-27 15:32:19 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿//Задача 1. Программа, которая выдает квадрат числа
+System.Console.Write("Введите число > ");
+String stringValue = Console.ReadLine();
+int value = Convert.ToInt32(stringValue);
+int result = value * value;
+System.Console.WriteLine($"Квадрат числа {value} равен {result}");
